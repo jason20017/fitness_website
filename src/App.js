7bef9bc66fcb8c3159c950c5767f2f03192cdf01
Jason Shiu <jason20017@hotmail.com>
2021-11-17 18:42:37 +0800

    renew

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <Nav />
       <Switch>
-        <Route path="/" exact>
+        <Route path="/">
           <Home />
         </Route>
         <Route path="/courses" exact>
